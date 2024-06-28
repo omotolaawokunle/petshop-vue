c455f953514c261f56c47d7c983ed26d8d95e673
Omotola Awokunle <omotolaawokunle@gmail.com>
2024-06-28 16:37:30 +0100

    first setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    
+    <p>"Welcome to Your Vue.js + TypeScript App"</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+  setup() {
+    const orders = ref([]);
+
+    return { orders };
+  }
 });
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#2EAE4E",
+      },
+    },
+  },
+  plugins: [],
+};
