9f26649e64ef62c6719853474be69d767cef1b55
Omotola Awokunle <omotolaawokunle@gmail.com>
2024-07-04 18:17:13 +0100

    Import tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import router from './router'
 import { createPinia } from 'pinia'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import "./main.css";
 
 const toastOptions = {
   position: "top-right",
   timeout: 5000,
   closeOnClick: true,
 };
-createApp(App).use(router).use(createPinia()).use(Toast, toastOptions).mount('#app')
+
+createApp(App).provide("baseUrl", "https://pet-shop.buckhill.com.hr/api/v1/").provide("formatDate", function (date: string) {
+  const newDate = new Date(date);
+  return newDate.toLocaleDateString();
+}).use(router).use(createPinia()).use(Toast, toastOptions).mount('#app')
